b72b997130561ec8fd13cb2e5b8a72ad78b76deb
Александр Мочалин <mochalin.av7@gmail.com>
2023-05-02 19:44:57 +0500

    v1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -16,13 +16,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'rest_framework_simplejwt',
     'django_filters',
     'api',
